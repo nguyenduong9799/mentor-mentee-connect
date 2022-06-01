9883fb78e525d707cde873565b9ecd54ff3f64d7
Endy <nguyenduong971999@gmail.com>
2022-06-01 12:08:56 +0700

    feat: update giao dien

--- lib/View/search.dart
+++ lib/View/search.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:mentor_mentee_connecting/utils/data.dart';
+import 'package:mentor_mentee_connecting/widgets/category_item.dart';
 import 'package:mentor_mentee_connecting/widgets/chat_item.dart';
 import 'package:mentor_mentee_connecting/widgets/custom_textfield.dart';
+import 'package:mentor_mentee_connecting/widgets/feature_item.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({Key? key}) : super(key: key);
@@ -21,7 +23,8 @@ class _SearchPageState extends State<SearchPage> {
       padding: EdgeInsets.only(left: 15, right: 15),
       child: Column(children: [
         getHeader(),
-        getChats(),
+        buildCategory(),
+        getSearchCourses(),
       ]),
     );
   }
@@ -56,15 +59,42 @@ class _SearchPageState extends State<SearchPage> {
         ));
   }
 
-  getChats() {
+  getSearchCourses() {
     return ListView(
         padding: EdgeInsets.only(top: 10),
         shrinkWrap: true,
         children: List.generate(
-            chats.length,
-            (index) => ChatItem(
-                  chats[index],
+            features.length,
+            (index) => FeatureItem(
                   onTap: () {},
+                  data: features[index],
+                  height: 240,
                 )));
+  }
+
+  int selectedCategoryIndex = 0;
+  Widget buildCategory() {
+    return SingleChildScrollView(
+      padding: const EdgeInsets.fromLTRB(15, 5, 7, 0),
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: List.generate(
+          categories.length,
+          (index) => Padding(
+            padding: const EdgeInsets.only(right: 8),
+            child: CategoryItem(
+              padding: const EdgeInsets.fromLTRB(15, 10, 15, 10),
+              data: categories[index],
+              isSelected: index == selectedCategoryIndex,
+              onTap: () {
+                setState(() {
+                  selectedCategoryIndex = index;
+                });
+              },
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/widgets/category_item.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:mentor_mentee_connecting/widgets/custom_image.dart';
+import '../theme/color.dart';
+
+class CategoryItem extends StatelessWidget {
+  const CategoryItem({
+    Key? key,
+    required this.data,
+    this.isSelected = false,
+    this.onTap,
+    this.padding = const EdgeInsets.fromLTRB(20, 15, 20, 15),
+  }) : super(key: key);
+  final dynamic data;
+  final bool isSelected;
+  final GestureTapCallback? onTap;
+  final EdgeInsets padding;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 500),
+        curve: Curves.fastOutSlowIn,
+        padding: padding,
+        decoration: BoxDecoration(
+          color: isSelected ? primary : cardColor,
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: shadowColor.withOpacity(0.07),
+              spreadRadius: .5,
+              blurRadius: .5,
+              offset: const Offset(0, 1), // changes position of shadow
+            ),
+          ],
+        ),
+        child: Container(
+          height: 28,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // CustomImage(
+              //   data["icon"],
+              //   width: 28,
+              //   height: 28,
+              //   radius: 5,
+              //   isNetwork: false,
+              //   trBackground: ,
+              //   isShadow: false,
+              // ),
+              // const SizedBox(
+              //   width: 7,
+              // ),
+              Text(
+                data["name"],
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                  fontSize: 18,
+                  color: isSelected ? textColor : darker,
+                  fontWeight: FontWeight.w500,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
